31156d7bddfc2dc132d28e6e8e602bdb34ac710a
Kalantar81 <Calantarov@gmail.com>
2020-11-30 12:35:28 +0200

    adding header, body, footer components

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderBodyFooterComponent } from './header-body-footer/header-body-footer.component';
 import { HeaderBodyComponent } from './header-body/header-body.component';
 import { BodyFooterComponent } from './body-footer/body-footer.component';
+import { HeaderComponent } from './view-parts/header/header.component';
+import { BodyComponent } from './view-parts/body/body.component';
+import { FooterComponent } from './view-parts/footer/footer.component';
 
 
 
@@ -10,7 +13,10 @@ import { BodyFooterComponent } from './body-footer/body-footer.component';
   declarations: [
     HeaderBodyFooterComponent,
     HeaderBodyComponent,
-    BodyFooterComponent
+    BodyFooterComponent,
+    HeaderComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
